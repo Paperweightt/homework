92478e585adb637c6d6417214e225f55e64b4311
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2023-09-12 14:27:49 -0400

    idk

---
+++ a2/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <ifstream>
+#include <string>
+using std::standard
+
+class Student {
+    public:
+        string name;
+        string scores;
+        string strScore;
+        string* scorePtr = &scores
+
+        Student() {
+
+        }
+        void Sort() {
+
+        }
+
+
+}
+
+
+
+
+int main() {
+    string line;
+    int i = 0
+    ifstream file ("student_scores.txt")
+
+
+    while() {
+        Student student()
+               
+        cout<< student.name
+        cout<< student.scores
+    }
+}
